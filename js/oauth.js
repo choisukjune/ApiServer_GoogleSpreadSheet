ef6a33a57c3a04c2927c2612bbb8a39bf480f952
choisukjune <sukjune.choi@gmail.com>
2023-02-09 23:06:32 +0900

    d

--- js/oauth.js
+++ js/oauth.js
@@ -99,7 +99,10 @@ var paramToObject = function( _url ){
 
 var parseJwt = function(token) {
 	var base64Url = token.split('.')[1];
+	console.log(base64Url)
 	var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+	console.log(base64)
+	console.log(atob)
 	var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
 		return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
 	}).join(''));
