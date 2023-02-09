7e774cbe14a89dc0eeea2d4c23b70909f0334e15
choisukjune <sukjune.choi@gmail.com>
2023-02-09 22:24:05 +0900

    fgfgfg

--- js/oauth.js
+++ js/oauth.js
@@ -154,7 +154,7 @@ var parseJwt = function(token) {
 		res.setHeader( "Access-Control-Allow-Origin", "*" );
 		res.setHeader( "Access-Control-Allow-Methods", "OPTIONS,POST,GET" );
 		console.log( _tDbjs_PATH + "/" + _tdbjs_nm + ".tdbjs" ); 
-		
+		var a = parseJwt(paramsO.credential)
 		try
 		{
 			var _tQuery = fs.readFileSync( _tDbjs_PATH + "/" + _tdbjs_nm + ".tdbjs" ).toString();
