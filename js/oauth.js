2d4f6bbf3884123b3e87e1815bebad2676d5b13e
choisukjune <sukjune.choi@gmail.com>
2023-02-18 14:50:52 +0900

    f

--- js/oauth.js
+++ js/oauth.js
@@ -168,7 +168,7 @@ var parseJwt = function(token) {
 			o[ _ta[0] ] = _ta[ 1 ];
 		}
 
-		var credentialInfo = parseJwt( o.credential )
+		var credentialInfo = parseJwt( data )
 		console.log( credentialInfo )
 		try
 		{
