111334d13f1f870a39c39091c651c3078604f64f
choisukjune <sukjune.choi@gmail.com>
2023-03-03 16:39:42 +0900

    asdfsadf

--- js/insert.js
+++ js/insert.js
@@ -355,6 +355,64 @@ var paramToObject = function( _url ){
 		res.end( r )	
 
 	});
+	/**
+	 * 쿼리파일을 실행하는 라우터
+	 * @function
+	 * @param {http.ClientRequest} req
+	 * <code>
+		{
+
+		}
+	* </code>
+	*
+	* @param {http.ClientResponse} res
+	* <code>
+		{
+
+		}
+	* </code>
+	*
+	* @example
+	* <code>
+		http://localhost:8888/insertBoard?data={...}
+	* </code>
+	*/
+	global.server.addRouter("/insertManyContainerData",function( req, res ){
+		
+		var routerNm = req.url.split("?")[0];
+		
+		var _tdbjs_nm = "insertManyContainerData";
+		
+		res.statusCode = 200;
+		res.setHeader( "Access-Control-Allow-Headers", "Content-Type" );
+		res.setHeader( "Access-Control-Allow-Origin", "*" );
+		res.setHeader( "Access-Control-Allow-Methods", "OPTIONS,POST,GET" );
+		console.log( _tDbjs_PATH + "/" + _tdbjs_nm + ".tdbjs" ); 
+		
+		try
+		{
+			var _tQuery = fs.readFileSync( _tDbjs_PATH + "/" + _tdbjs_nm + ".tdbjs" ).toString();
+		}
+		catch( err )
+		{
+			console.log( routerNm + " - DBJS File Not Found! - " + err );
+			res.end("{ sucess : 0, data : null }");
+		}
+		delete data[ "$height" ];
+		var query = _tQuery.replace( "<!=DATA=!>", JSON.stringify( data ) );
+		var dbjs_nm = "insertManyContainerData.dbjs";
+
+		var FILE_PATH = DBJS_DIRECTORY_PATH + dbjs_nm;
+		
+		console.log( FILE_PATH )
+
+		fs.writeFileSync( DBJS_DIRECTORY_PATH + dbjs_nm , query, { flag : "w" } );
+
+		var r = exec_query_DB( dbjs_nm )
+        console.log( r )
+		res.end( r );
+
+	});
 })();
 
 //-------------------------------------------------------;
